a2d0e9eeabf36e1f2725652d507cc2685208adde
diogro <diogro@gmail.com>
2015-04-07 17:04:58 -0300

    proj

--- bipartiteGraph.R
+++ bipartiteGraph.R
@@ -10,23 +10,30 @@ divsel = ReadRawPop(folder = "/home/diogro/projects/evomod/c-gsl/output/burn_in/
 plotAdj <- function (bg) {
   pr=bipartite.projection(bg)
   l <-layout.reingold.tilford(pr$proj2) 
-  l[1:5,1] = 0
+  l[1:5,1] = c(0, -1, -2, -1, 0)
   l[1:5,2] = 1:5
-  l[6:10,1] = 1
+  l[6:10,1] = c(1, 2, 3, 2, 1)
   l[6:10,2] = 1:5  
   plot(pr$proj2, 
        edge.width=E(pr$proj2)$weight^2/30000,
        edge.color="black", 
        vertex.label=V(pr$proj2)$name,
        layout = l)
-  return(get.adjacency(pr$proj2,sparse=FALSE,attr="weight"))
+  return(get.adjacency(pr$proj1,sparse=FALSE,attr="weight"))
 }
 
-bg = graph.incidence(burn$B[,,1])
-dg = graph.incidence(divsel$B[,,1])
+bg = graph.incidence(burn$B[,,2])
+dg = graph.incidence(divsel$B[,,2])
 b = plotAdj(bg)
 d = plotAdj(dg)
 mod = cbind(rep(c(1, 0), each = 5),
             rep(c(0, 1), each = 5))
 TestModularity(b, mod)
 TestModularity(d, mod)
+divsel$G
+
+da = graph.adjacency(d, mode= "undirected", weighted = TRUE)
+gg.com <- fastgreedy.community(da)
+V(da)$color <- gg.com$membership + 1
+plot(da)
+
