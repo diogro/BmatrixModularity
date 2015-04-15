d5e0581c27860bc29999209ec464605be498f781
diogro <diogro@gmail.com>
2015-04-15 17:36:46 -0300

    stab

--- bipartiteGraph.R
+++ bipartiteGraph.R
@@ -3,8 +3,13 @@ library(evolqg)
 library(EvomodR)
 
 
-burn = ReadRawPop(folder = "/home/diogro/projects/large_projects/evomod/c-gsl/output/burn_in", "burnin")
-divsel = ReadRawPop(folder = "/home/diogro/projects/large_projects/evomod/c-gsl/output/burn_in/DivSel-0.004", "DivSel-0.004")
+burn = ReadRawPop(folder = "/home/diogro/projects/evomod/c-gsl/output/burn_in", "burnin")
+stab = ReadRawPop(folder = "/home/diogro/projects/evomod/c-gsl/output/CorrelatedBurin_Results/burn_in/", "stab")
+divsel = ReadRawPop(folder = "/home/diogro/projects/evomod/c-gsl/output/burn_in/DivSel-0.004", "DivSel-0.004")
+
+
+divsel$B[,,2] %*% t(divsel$B[,,2])
+t(divsel$B[,,2]) %*% divsel$B[,,2]
 
 
 plotAdj <- function (bg) {
@@ -14,31 +19,40 @@ plotAdj <- function (bg) {
   l[1:5,2] = 1:5
   l[6:10,1] = c(1, 2, 3, 2, 1)
   l[6:10,2] = 1:5  
+  gg.com <- fastgreedy.community(pr$proj2)
+  V(pr$proj2)$color <- gg.com$membership + 1
   plot(pr$proj2, 
        edge.width=(E(pr$proj2)$weight/200)^2,
        edge.color="black", 
        vertex.label=V(pr$proj2)$name,
+       vertex.color = V(pr$proj2)$color,
        layout = l)
   return(pr)
 }
 
-bg = graph.incidence(burn$B[,,2])
-dg = graph.incidence(divsel$B[,,2])
+bg = graph.incidence(burn$B[,,3])
+sg = graph.incidence(stab$B[,,3])
+dg = graph.incidence(divsel$B[,,3])
 b = plotAdj(bg)
+s = plotAdj(sg)
 d = plotAdj(dg)
+
 
 hist(apply(burn$B, 1:2, mean))
 hist(apply(divsel$B, 1:2, mean))
 
+get.adjacency(b$proj2,sparse=FALSE,attr="weight")
+get.adjacency(s$proj2,sparse=FALSE,attr="weight")
 get.adjacency(d$proj2,sparse=FALSE,attr="weight")
+
 
 mod = cbind(rep(c(1, 0), each = 5),
             rep(c(0, 1), each = 5))
 # TestModularity(b, mod)
 # TestModularity(d, mod)
 
-da = graph.adjacency(d, mode= "undirected", weighted = TRUE)
-gg.com <- fastgreedy.community(da)
-V(da)$color <- gg.com$membership + 1
+da = graph.adjacency(d$proj2, mode= "undirected", weighted = TRUE)
+gg.com <- fastgreedy.community(d$proj2)
+V(d)$color <- gg.com$membership + 1
 plot(da)
 
